e8bade3c51b4d54bf32ccaaea39143f84fec0152
Haaai <haixiang@usc.edu>
2021-08-04 13:57:35 +0800

    add special addr for staking actions

--- address/address_special.go
+++ address/address_special.go
@@ -9,14 +9,38 @@ const (
 	// StakingBucketPoolAddr is the staking bucket pool address
 	StakingBucketPoolAddr = "io000000000000000000000000stakingprotocol"
 
+	// StakingCreateAddr is the special address for staking create actions
+	StakingCreateActionAddr = "io000000000000000000000000web3stakecreate"
+
+	// StakingAddDepositActionAddr is the special address for staking add deposit actions
+	StakingAddDepositActionAddr = "io00000000000000000000web3stakeadddeposit"
+
+	// StakingChangeCandidateActionAddr is the special address for staking change candidate actions
+	StakingChangeCandidateActionAddr = "io000000000000000web3stakechangecandidate"
+
+	// StakingReclaimActionAddr is the special address for staking reclaim actions
+	StakingReclaimActionAddr = "io00000000000000000000000web3stakereclaim"
+
+	// StakingRestakeActionAddr is the special address for staking restake actions
+	StakingRestakeActionAddr = "io00000000000000000000000web3stakerestake"
+
+	// StakingTransferStakeAddr is the special address for staking transfer stake actions
+	StakingTransferStakeAddr = "io00000000000000000web3staketransferstake"
+
 	// RewardingPoolAddr is the rewarding pool address
 	RewardingPoolAddr = "io0000000000000000000000rewardingprotocol"
 )
 
 // 20-byte protocol address hash
 var (
-	StakingProtocolAddrHash   = hash160b([]byte("staking"))
-	RewardingProtocolAddrHash = hash160b([]byte("rewarding"))
+	StakingProtocolAddrHash              = hash160b([]byte("staking"))
+	RewardingProtocolAddrHash            = hash160b([]byte("rewarding"))
+	StakingCreateActionAddrHash          = hash160b([]byte("stakingCreateAct"))
+	StakingAddDepositActionAddrHash      = hash160b([]byte("stakingAddDepositAct"))
+	StakingChangeCandidateActionAddrHash = hash160b([]byte("stakingChangeCandidateAct"))
+	StakingReclaimActionAddrHash         = hash160b([]byte("stakingReclaimAct"))
+	StakingRestakeActionAddrHash         = hash160b([]byte("stakingRestakeAct"))
+	StakingTransferStakeActionAddrHash   = hash160b([]byte("stakingTransferStakeAct"))
 )
 
 type (
