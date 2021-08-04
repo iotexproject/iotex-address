89f8fc0e6947c66d46a8eac955c3fba6d39f7383
dustinxie <dahuaxie@gmail.com>
2021-08-04 15:45:04 -0700

    Update address_v1_test.go

--- address/address_v1_test.go
+++ address/address_v1_test.go
@@ -83,30 +83,40 @@ func TestAddressError(t *testing.T) {
 func TestSpecialAddress(t *testing.T) {
 	require := require.New(t)
 
-	tests := []struct {
-		specialAddr string
-		hashByte    Hash160
-		hash        string
-	}{
-		{StakingBucketPoolAddr, StakingProtocolAddrHash, "io1qnpz47hx5q6r3w876axtrn6yz95d70cjl35r53"},
-		{StakingCreateAddr, StakingCreateAddrHash, "io16tmc6yd9kn0z90axeack8r0sd9dsr0p8lve54d"},
-		{StakingAddDepositAddr, StakingAddDepositAddrHash, "io1l08h9492aphd5p7e6mc22e352dwpc62qwj43za"},
-		{StakingChangeCandidateAddr, StakingChangeCandidateAddrHash, "io1332snsr9jnpdlsmhprmxsktdq6q3a3yh0wu8uf"},
-		{StakingUnstakeAddr, StakingUnstakeAddrHash, "io1mdr5s62d0pww8pn0zjx4xne7u34z66ml53du45"},
-		{StakingWithdrawAddr, StakingWithdrawAddrHash, "io1sa8nv8s0u4h0ngvhavl0lzqnrq88a6dymkuxv7"},
-		{StakingRestakeAddr, StakingRestakeAddrHash, "io1fg8hwmdgkm4ytrx4p7mlx0sak0wmq6az96lnlk"},
-		{StakingTransferStakeAddr, StakingTransferStakeAddrHash, "io12v9rhxl8xr0neh9x2uwyu5xhyl043048ctdhs7"},
-		{StakingCandidateRegisterAddr, StakingCandidateRegisterAddrHash, "io1yrwqura2ww2gt00gmf79jdkuh8hq0kufufdsss"},
-		{StakingCandidateUpdateAddr, StakingCandidateUpdateAddrHash, "io15q33lxah8u5g97nh02r5x3c2x22pj5p0zavtd3"},
-		{RewardingPoolAddr, RewardingProtocolAddrHash, "io154mvzs09vkgn0hw6gg3ayzw5w39jzp47f8py9v"},
-	}
-	const ADDRLENGTH int = 41
+	addr1, err := _v1.FromBytes(StakingProtocolAddrHash[:])
+	require.NoError(err)
+	require.Equal("io1qnpz47hx5q6r3w876axtrn6yz95d70cjl35r53", addr1.String())
+	addr1, err = _v1.FromBytes(RewardingProtocolAddrHash[:])
+	require.NoError(err)
+	require.Equal("io154mvzs09vkgn0hw6gg3ayzw5w39jzp47f8py9v", addr1.String())
 
+	// special address has same length
+	require.Equal(41, len(StakingBucketPoolAddr))
+	require.Equal(41, len(RewardingPoolAddr))
+
+	// but cannot decode
+	addr1, err = _v1.FromString(StakingBucketPoolAddr)
+	require.Error(err)
+	addr1, err = _v1.FromString(RewardingPoolAddr)
+	require.Error(err)
+
+	// special address for staking actions
+	tests := []struct {
+		hashByte Hash160
+		addr     string
+	}{
+		{StakingCreateAddrHash, "io1qqqqqqqqqqq8xarpdd5kue6rwfjkzar9k0wk6t"},
+		{StakingAddDepositAddrHash, "io1qqqqqum5v94kjmn8g9jxg3r9wphhx6t58x7tye"},
+		{StakingChangeCandAddrHash, "io1qqqqqum5v94kjmn8gd5xzmn8v4pkzmnye5v3fh"},
+		{StakingUnstakeAddrHash, "io1qqqqqqqqqpehgcttd9hxw4twwd6xz6m9pl4r27"},
+		{StakingWithdrawAddrHash, "io1qqqqqqqqwd6xz6mfden4w6t5dpj8ycthwsq5ng"},
+		{StakingRestakeAddrHash, "io1qqqqqqqqqpehgcttd9hxw5n9wd6xz6m995w4zm"},
+		{StakingTransferAddrHash, "io1qqqqqqqqwd6xz6mfden4gunpdeekvetjzwh99y"},
+		{StakingRegisterCandAddrHash, "io1qpehgcttd9hxw5n9va5hxar9wfpkzmnyahxhjk"},
+		{StakingUpdateCandAddrHash, "io1qqqqqum5v94kjmn824cxgct5v4pkzmnyxxj98n"},
+	}
 	for _, test := range tests {
-		require.Equal(ADDRLENGTH, len(test.specialAddr))
-		_, err := _v1.FromString(test.specialAddr)
-		require.Error(err)
 		addr, _ := _v1.FromBytes(test.hashByte[:])
-		require.Equal(test.hash, addr.String())
+		require.Equal(test.addr, addr.String())
 	}
 }
