3f62ca250e8780046067381022b7746b57292597
Dustin Xie <dahuaxie@gmail.com>
2021-11-21 19:09:15 -0800

    handle special address

--- address/address_special.go
+++ address/address_special.go
@@ -11,6 +11,39 @@ const (
 
 	// RewardingPoolAddr is the rewarding pool address
 	RewardingPoolAddr = "io0000000000000000000000rewardingprotocol"
+
+	// StakingProtocolAddr is the staking protocol address
+	StakingProtocolAddr = "io1qnpz47hx5q6r3w876axtrn6yz95d70cjl35r53"
+
+	// RewardingProtocol is the rewarding protocol address
+	RewardingProtocol = "io154mvzs09vkgn0hw6gg3ayzw5w39jzp47f8py9v"
+
+	// StakingCreateAddr is the staking create address
+	StakingCreateAddr = "io1qqqqqqqqqqq8xarpdd5kue6rwfjkzar9k0wk6t"
+
+	// StakingAddDepositAddr is the staking add deposit address
+	StakingAddDepositAddr = "io1qqqqqum5v94kjmn8g9jxg3r9wphhx6t58x7tye"
+
+	// StakingChangeCandAddr is the staking change candidate address
+	StakingChangeCandAddr = "io1qqqqqum5v94kjmn8gd5xzmn8v4pkzmnye5v3fh"
+
+	// StakingUnstakeAddr is the staking unstake address
+	StakingUnstakeAddr = "io1qqqqqqqqqpehgcttd9hxw4twwd6xz6m9pl4r27"
+
+	// StakingWithdrawAddr is the staking withdraw address
+	StakingWithdrawAddr = "io1qqqqqqqqwd6xz6mfden4w6t5dpj8ycthwsq5ng"
+
+	// StakingRestakeAddr is the staking restake address
+	StakingRestakeAddr = "io1qqqqqqqqqpehgcttd9hxw5n9wd6xz6m995w4zm"
+
+	// StakingTransferAddr is the staking transfer address
+	StakingTransferAddr = "io1qqqqqqqqwd6xz6mfden4gunpdeekvetjzwh99y"
+
+	// StakingRegisterCandAddr is the staking register candidate address
+	StakingRegisterCandAddr = "io1qpehgcttd9hxw5n9va5hxar9wfpkzmnyahxhjk"
+
+	// StakingUpdateCandAddr is the staking update candidate address
+	StakingUpdateCandAddr = "io1qqqqqum5v94kjmn824cxgct5v4pkzmnyxxj98n"
 )
 
 // 20-byte protocol address hash
@@ -26,11 +59,6 @@ var (
 	StakingTransferAddrHash     = bytesToHash160([]byte("stakingTransfer"))
 	StakingRegisterCandAddrHash = bytesToHash160([]byte("stakingRegisterCand"))
 	StakingUpdateCandAddrHash   = bytesToHash160([]byte("stakingUpdateCand"))
-)
-
-type (
-	// Hash160 for 160-bit hash used for account and smart contract address
-	Hash160 [20]byte
 )
 
 // hash160b returns 160-bit (20-byte) hash of input
@@ -51,4 +79,39 @@ func bytesToHash160(b []byte) Hash160 {
 	}
 	copy(h[20-len(b):], b)
 	return h
+}
+
+// AddrV1Special is address that consists of special text
+// it is NOT a valid bech32 encoding of the 20-bytes hash
+type AddrV1Special struct {
+	addr string
+}
+
+func newAddrV1Special(s string) *AddrV1Special {
+	return &AddrV1Special{
+		addr: s,
+	}
+}
+
+// IsAddrV1Special returns true for special address
+func IsAddrV1Special(s string) bool {
+	switch s {
+	case RewardingPoolAddr, StakingBucketPoolAddr:
+		return true
+	default:
+		return false
+	}
+}
+
+// String returns the special-text address
+func (addr *AddrV1Special) String() string { return addr.addr }
+
+// Bytes panics since it is NOT a valid bech32 encoding
+func (addr *AddrV1Special) Bytes() []byte {
+	panic("Bytes() does not apply for special address")
+}
+
+// Hex panics since it is NOT a valid bech32 encoding
+func (addr *AddrV1Special) Hex() string {
+	panic("Hex() does not apply for special address")
 }
--- address/address_v1_test.go
+++ address/address_v1_test.go
@@ -47,9 +47,14 @@ func TestAddress(t *testing.T) {
 		require.Equal("0x", addrHex[:2])
 		addr2, err = FromHex(addrHex)
 		require.NoError(err)
-		require.True(Equal(addr1, addr2))
-		_, err = FromHex(addrHex[:len(addrHex)-2])
-		require.Error(err)
+		require.Equal(addr1, addr2)
+		addr2, err = FromHex(addrHex[2:])
+		require.NoError(err)
+		require.Equal(addr1, addr2)
+		// remove the last byte
+		addr2, err = FromHex(addrHex[:len(addrHex)-2])
+		require.NoError(err)
+		require.Equal(addr2.Hex()[4:], addrHex[2:len(addrHex)-2])
 	}
 	t.Run("testnet", func(t *testing.T) {
 		require.NoError(os.Setenv("IOTEX_NETWORK_TYPE", "testnet"))
@@ -83,40 +88,46 @@ func TestAddressError(t *testing.T) {
 func TestSpecialAddress(t *testing.T) {
 	require := require.New(t)
 
-	addr1, err := _v1.FromBytes(StakingProtocolAddrHash[:])
-	require.NoError(err)
-	require.Equal("io1qnpz47hx5q6r3w876axtrn6yz95d70cjl35r53", addr1.String())
-	addr1, err = _v1.FromBytes(RewardingProtocolAddrHash[:])
-	require.NoError(err)
-	require.Equal("io154mvzs09vkgn0hw6gg3ayzw5w39jzp47f8py9v", addr1.String())
-
-	// special address has same length
-	require.Equal(41, len(StakingBucketPoolAddr))
-	require.Equal(41, len(RewardingPoolAddr))
-
-	// but cannot decode
-	addr1, err = _v1.FromString(StakingBucketPoolAddr)
-	require.Error(err)
-	addr1, err = _v1.FromString(RewardingPoolAddr)
-	require.Error(err)
+	for _, v := range []string{
+		StakingBucketPoolAddr,
+		RewardingPoolAddr,
+	} {
+		require.Equal(41, len(v))
+		require.True(IsAddrV1Special(v))
+		addr, err := _v1.FromString(v)
+		require.NoError(err)
+		require.Equal(v, addr.String())
+		require.Panics(func() {
+			addr.Bytes()
+		})
+		require.Panics(func() {
+			addr.Hex()
+		})
+	}
 
 	// special address for staking actions
 	tests := []struct {
 		hashByte Hash160
 		addr     string
 	}{
-		{StakingCreateAddrHash, "io1qqqqqqqqqqq8xarpdd5kue6rwfjkzar9k0wk6t"},
-		{StakingAddDepositAddrHash, "io1qqqqqum5v94kjmn8g9jxg3r9wphhx6t58x7tye"},
-		{StakingChangeCandAddrHash, "io1qqqqqum5v94kjmn8gd5xzmn8v4pkzmnye5v3fh"},
-		{StakingUnstakeAddrHash, "io1qqqqqqqqqpehgcttd9hxw4twwd6xz6m9pl4r27"},
-		{StakingWithdrawAddrHash, "io1qqqqqqqqwd6xz6mfden4w6t5dpj8ycthwsq5ng"},
-		{StakingRestakeAddrHash, "io1qqqqqqqqqpehgcttd9hxw5n9wd6xz6m995w4zm"},
-		{StakingTransferAddrHash, "io1qqqqqqqqwd6xz6mfden4gunpdeekvetjzwh99y"},
-		{StakingRegisterCandAddrHash, "io1qpehgcttd9hxw5n9va5hxar9wfpkzmnyahxhjk"},
-		{StakingUpdateCandAddrHash, "io1qqqqqum5v94kjmn824cxgct5v4pkzmnyxxj98n"},
+		{Hash160{}, ZeroAddress},
+		{StakingProtocolAddrHash, StakingProtocolAddr},
+		{RewardingProtocolAddrHash, RewardingProtocol},
+		{StakingCreateAddrHash, StakingCreateAddr},
+		{StakingAddDepositAddrHash, StakingAddDepositAddr},
+		{StakingChangeCandAddrHash, StakingChangeCandAddr},
+		{StakingUnstakeAddrHash, StakingUnstakeAddr},
+		{StakingWithdrawAddrHash, StakingWithdrawAddr},
+		{StakingRestakeAddrHash, StakingRestakeAddr},
+		{StakingTransferAddrHash, StakingTransferAddr},
+		{StakingRegisterCandAddrHash, StakingRegisterCandAddr},
+		{StakingUpdateCandAddrHash, StakingUpdateCandAddr},
 	}
 	for _, test := range tests {
+		require.False(IsAddrV1Special(test.addr))
 		addr, _ := _v1.FromBytes(test.hashByte[:])
 		require.Equal(test.addr, addr.String())
+		addr, _ = _v1.FromString(test.addr)
+		require.Equal(test.hashByte[:], addr.Bytes())
 	}
 }
